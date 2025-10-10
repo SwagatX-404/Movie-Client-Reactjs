1851500a386ba01a40f994b7d9f1fcac568129ca
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-10-11 02:37:43 +0530

    set axios backend  base URL, api.get from localhost8080/api/v1/movies - springboot java backend and fix core issue in SpBoot

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,17 @@
 import axios from 'axios';
 
 
+// Create an Axios instance with a base URL and custom headers
+// Replace the baseURL with your actual backend server URL
+// The header "ngrok-skip-browser-warning" is used to bypass ngrok's browser warning
+// if you're using ngrok for local development
+//by pass cores origin policy issue
+// export default axios.create({
+//     baseURL:'https://9c96-103-106-239-104.ap.ngrok.io',
+//     headers: {"ngrok-skip-browser-warning": "true"}
+// });
+
 export default axios.create({
-    
+  baseURL: 'http://localhost:8080',
+  headers: { 'Content-Type': 'application/json' }
 });
