e862c480a6e20ff8b9e1bc3f9307e246b26bda1d
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-10-12 22:23:09 +0530

    styling hero.js

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -3,19 +3,19 @@ import './Hero.css';
 import Carousel from 'react-material-ui-carousel';
 import { Paper } from '@mui/material';
 
-const Hero = ({movies}) => {
+const Hero = ({movies =[]}) => {
   return (
     <div>
-        <Carousel>
+        <Carousel className='movie-carosel-container' >
             {
                 movies.map((movie) =>{
                     return(
                         <Paper>
                             <div className='movie-card-container'>
-                                <div className='movie-card'>
+                                <div className='movie-card' style={{"--img": `url(${movie.backdrops[0]})`}}>
                                     <div className='movie-detail'>
                                         <div className='movie-poster'>
-                                            <img src={movie.poster} alt='MoviePoster'/>
+                                            <img src={movie.poster} alt={`${movie.title} Poster`}/>
                                         </div>
                                         <div className='movie-title'>
                                             <h4>{movie.title}</h4>
