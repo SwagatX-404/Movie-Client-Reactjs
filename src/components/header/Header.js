1096a5777d2e50fd759b53101b6ef9ffa18bd552
Swagat Murmu <153312889+SwagatX-404@users.noreply.github.com>
2025-10-14 23:43:47 +0530

    add nav bar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,27 +1,38 @@
 import React from 'react'
+import './Header.css';
 import mov from './mov.png'    // import your image (since it’s in the same folder)
 
 const Header = () => {
   return (
     <div>
-        <nav>
-              <div>
-                  <img src={mov} alt="Movie Logo" className="logo" width="60px" />
-                  <h2>S M◉vies</h2>
+        <nav className='nav-container'>
+              <div className='logo-con'>
+                  {/* <img src={mov} alt="Movie Logo" className="logo" width="50px" /> */}
+                  <h2>M◉VIES</h2>
               </div>
               <div>
-                <ul>
+                <ul className='li-container'>
                     <li>HOME</li>
                     <li>MOVIES</li>
                     <li>TV SHOWS</li>
                 </ul>
               </div>
               <div>
-                <ul>
-                    <li>Serach</li>
-                    <li>Log In</li>
+                <div className='li-container'>
+                    <div>
+                      <div class="searchInput">
+                            <input className = 'inp' type="text" name="" placeholder="Search"/>
+                        <button class="searchButton" href="#"> 
+                               &#128269;
+                      </button>
+                      </div>
+                      
+                    </div>
+                   <div className='login'>
+                    <div>LOG IN</div>
+                   </div>
 
-                </ul>
+                </div>
               </div>
 
         </nav>
